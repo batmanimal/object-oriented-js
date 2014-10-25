a4d4b6c095523e8fc1b33c55fd04af040bcd73c9
thejenniekim <jk@udacity.com>
2014-10-25 16:07:21 -0700

    completed subclasses

---
+++ Superclass and Subclasses.js
@@ -0,0 +1,40 @@
+// Superclass and Subclasses
+// Class function
+var Car = function (loc) {
+	var obj = {loc: loc};
+	obj.move = function(){
+		obj.loc++;
+	};
+	return obj;
+};
+
+// Create a second category of object for cal (a Cop object), with same powers as category
+var cal = Car(2);
+// cal.call() // property does not exist in Car, special to Cop. Let's add it!
+
+// Create superclass that produces an object that starts where the superclass leaves off
+var SuperCar = function () {
+	var obj = {loc: loc};
+	obj.move = function () {
+		obj.loc++;
+	};
+	return obj;
+};
+
+// Subclass Van
+var Van = function(loc) {
+	var obj = Car(loc);
+	obj.grab = function () {
+// some code here
+	};
+	return obj;
+};
+
+// Subclass Cop
+var Cop = function(loc) {
+	var obj = Car(loc);
+	obj.call = function () {
+		// some code
+	};
+	return obj;
+};
