094c5143cbdc395aef08d4eb1c45b98ae80aaff2
thejenniekim <jk@udacity.com>
2014-10-28 17:28:16 -0700

    added closures

---
+++ 2-Closures.js
@@ -0,0 +1,17 @@
+var sagas = []; // global array to store saga function objects, accessible outside of the newSaga function 
+var hero = aHero();
+var newSaga = function(){
+	var foil = aFoil();
+	//var saga = function(){   -> now need to push saga function objects into the sagas array 
+	sagas.push(function(){  
+		var deed = aDeed();
+		log(hero+deed+foil);
+	});
+};
+newSaga();
+sagas[0](); // invokes the first function stored in sagas array
+sagas[0]();
+
+newSaga();
+sagas[1]();
+
