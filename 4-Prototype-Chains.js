90b8a4c25e1e96a187f468f855b46c8661b380ac
Jennie Kim <jk@udacity.com>
2014-11-12 15:37:48 -0800

    Update 4-Prototype-Chains.js

--- 4-Prototype-Chains.js
+++ 4-Prototype-Chains.js
@@ -1,3 +1,6 @@
+// HOW TO USE THESE NOTES: 
+// Use these notes to follow along with the lesson. You can run the code in this file to reproduce what you see in the videos. 
+
 // Prototype Chains
 
 var gold = {a:1};
