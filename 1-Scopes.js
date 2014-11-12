35244e857f0361a446d2061e243d1c38b5f16752
Jennie Kim <jk@udacity.com>
2014-11-12 15:36:05 -0800

    added better comments

--- 1-Scopes.js
+++ 1-Scopes.js
@@ -1,11 +1,13 @@
+// HOW TO USE THESE NOTES: 
+// Use these notes to follow along with the lesson. You can run the code in this file to reproduce what you see in the videos. 
+
 // Scopes 
 // Lexical scope: regions in your source code where you can 
 // refer to a variable by name without getting access errors
 // Blocks within a function create a new lexical scope 
 
-// Global Scope
-// Establish dummy functions to return some words 
-function aHero () {
+// Global Scope starts under here 
+function aHero () {  // Establish dummy functions to return some words so the function calls in local scopes work
 	return "Gal";
 }
 function aFoil () {
@@ -16,17 +18,17 @@ function aDeed () {
 }
 
 var hero = aHero();
-var newSaga = function() { // scope1 starts here
+var newSaga = function() { // <-- scope1 starts here
 	var foil = aFoil();
-	var saga = function() { // scope2 starts here 
+	var saga = function() { // <-- scope2 starts here 
 		var deed = aDeed();
 		console.log(hero+deed+foil);
-	}; // scope2 ends here 
+	}; // <-- scope2 ends here 
 	saga();
 	// -> GalTapsCow
 	saga();
 	// -> GalTapsCow
-}; // scope1 ends here
+}; // <-- scope1 ends here
 newSaga();
 // -> GalTapsCow
 newSaga();
